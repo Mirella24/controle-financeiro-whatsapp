8da70ebb2fa22f612b7e9333d8878f628e6e3c1a
Mirella24 <mirella.mares24@gmail.com>
2026-03-25 15:37:34 -0300

    teste definitivo server direto

--- src/server.js
+++ src/server.js
@@ -1,4 +1,20 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('SERVER TESTE FINAL');
+});
+
+app.post('/twilio/whatsapp', (req, res) => {
+  return res.status(200).json({
+    ok: true,
+    origem: 'server.js DIRETO',
+    body: req.body
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
